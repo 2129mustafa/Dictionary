c818b6d5470717970d5964df5842c0bc91a063ed
MUSTAFA YENAL <202803018@ogr.cbu.edu.tr>
2021-04-03 17:52:39 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T, M>
+    {
+        T[] t_items;
+        M[] m_items;
+        //constroctur
+        public MyDictionary()
+        {
+            t_items = new T[0];
+            m_items = new M[0];
+        }
+
+        public void Add(T t_item, M m_item)
+        {
+            T[] t_array = t_items;  //gecici diziye aktardım
+            M[] m_array = m_items;
+
+            t_items = new T[t_items.Length + 1];
+            m_items = new M[m_items.Length + 1];
+
+            for (int i = 0; i < t_array.Length; i++)
+            {
+                t_items[i] = t_array[i];
+            }
+            t_items[t_items.Length - 1] = t_item;
+
+            for (int j = 0; j < m_array.Length; j++)
+            {
+                m_items[j] = m_array[j];
+            }
+            m_items[m_items.Length - 1] = m_item;
+
+        }
+        public int Count
+        {
+            get { return t_items.Length; }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new Dictionary.MyDictionary<int, string>();
+
+            myDictionary.Add(456, "Mustafa");
+            myDictionary.Add(789, "Burak");
+            myDictionary.Add(123, "Halil");
+            myDictionary.Add(852, "Ali");
+            myDictionary.Add(258, "Erdem");
+
+            Console.WriteLine(myDictionary.Count);
+
+        }
+    }
+}
